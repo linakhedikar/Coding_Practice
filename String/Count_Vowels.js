e8e19036c00a6d757b80d0797ef38431c32ceec6
linakhedikar <linakhedikar151@gmail.com>
2026-08-15 00:02:00 +0530

    basic string problems added

---
+++ String/Count_Vowels.js
@@ -0,0 +1,17 @@
+//Count the number of Vowels in a string.
+function countVowels(str) {
+  let count = 0;
+  for (let char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countVowels("javascript"));
